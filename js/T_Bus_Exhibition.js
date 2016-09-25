70e53a678f9fc9acbc85552be52bbe42a136e0ef
asxsyd92 <asxsyd92@foxmail.com>
2016-09-25 21:10:20 +0800

    1

--- js/T_Bus_Exhibition.js
+++ js/T_Bus_Exhibition.js
@@ -3,7 +3,7 @@
     "Code": "1-1",
     "Type": "zjl",
     "Name": "精品柜",
-    "PicUrl": "../uploads/product.gif",
+    "PicUrl": "../uploads/1-1.png",
     "Ex_Len": "1000",
     "Ex_Width": "500",
     "Ex_Height": "1800",
@@ -15,9 +15,9 @@
 {
     "KeyId": "2",
     "Code": "1-2",
-    "Type": "login.html",
+    "Type": "zjl",
     "Name": "双立柜",
-    "PicUrl": "../uploads/product.gif",
+    "PicUrl": "../uploads/1-2.png",
     "Ex_Len": "1000",
     "Ex_Width": "500",
     "Ex_Height": "1800",
@@ -28,90 +28,538 @@
 }, {
 
     "KeyId": "3",
-    "Code": "1-2",
-    "Type": "login.html",
-    "Name": "双立柜",
-    "PicUrl": "../uploads/product.gif",
-    "Ex_Len": "1000",
+    "Code": "1-3",
+    "Type": "zjl",
+    "Name": "方立柜",
+    "PicUrl": "../uploads/1-3.png",
+    "Ex_Len": "500",
     "Ex_Width": "500",
     "Ex_Height": "1800",
     "Unit": "个",
-    "RentPrice": "350元/个/会期",
-    "Deposit": "350元/个",
+    "RentPrice": "300元/个/会期",
+    "Deposit": "300元/个",
     "Description": ""
 }, {
 
     "KeyId": "4",
-    "Code": "1-2",
-    "Type": "login.html",
-    "Name": "双立柜",
-    "PicUrl": "../uploads/product.gif",
+    "Code": "1-4",
+    "Type": "zjl",
+    "Name": "1米旧单台柜",
+    "PicUrl": "../uploads/1-4.png",
     "Ex_Len": "1000",
     "Ex_Width": "500",
-    "Ex_Height": "1800",
+    "Ex_Height": "900",
     "Unit": "个",
-    "RentPrice": "350元/个/会期",
-    "Deposit": "350元/个",
+    "RentPrice": "200元/个/会期",
+    "Deposit": "200元/个",
     "Description": ""
 }
 , {
 
     "KeyId": "5",
-    "Code": "1-2",
-    "Type": "login.html",
-    "Name": "双立柜",
-    "PicUrl": "../uploads/product.gif",
-    "Ex_Len": "1000",
+    "Code": "1-5",
+    "Type": "zjl",
+    "Name": "1.2米旧单台柜",
+    "PicUrl": "../uploads/1-5.png",
+    "Ex_Len": "1200",
     "Ex_Width": "500",
-    "Ex_Height": "1800",
+    "Ex_Height": "900",
     "Unit": "个",
-    "RentPrice": "350元/个/会期",
-    "Deposit": "350元/个",
+    "RentPrice": "200元/个/会期",
+    "Deposit": "200元/个",
     "Description": ""
 }
 , {
 
     "KeyId": "6",
-    "Code": "1-2",
-    "Type": "login.html",
-    "Name": "双立柜",
-    "PicUrl": "../uploads/product.gif",
+    "Code": "1-6",
+    "Type": "zjl",
+    "Name": "新单台柜",
+    "PicUrl": "../uploads/1-6.png",
     "Ex_Len": "1000",
     "Ex_Width": "500",
-    "Ex_Height": "1800",
+    "Ex_Height": "1000",
     "Unit": "个",
-    "RentPrice": "350元/个/会期",
-    "Deposit": "350元/个",
+    "RentPrice": "300元/个/会期",
+    "Deposit": "300元/个",
     "Description": ""
 }
 , {
 
     "KeyId": "7",
-    "Code": "1-2",
-    "Type": "login.html",
-    "Name": "双立柜",
-    "PicUrl": "../uploads/product.gif",
+    "Code": "1-7",
+    "Type": "zjl",
+    "Name": "折叠单台柜",
+    "PicUrl": "../uploads/1-7.png",
     "Ex_Len": "1000",
     "Ex_Width": "500",
-    "Ex_Height": "1800",
+    "Ex_Height": "1000",
     "Unit": "个",
-    "RentPrice": "350元/个/会期",
-    "Deposit": "350元/个",
+    "RentPrice": "300元/个/会期",
+    "Deposit": "300元/个",
     "Description": ""
 }
 , {
 
     "KeyId": "8",
-    "Code": "1-2",
-    "Type": "login.html",
-    "Name": "双立柜",
-    "PicUrl": "../uploads/product.gif",
+    "Code": "1-8",
+    "Type": "zjl",
+    "Name": "咨询柜",
+    "PicUrl": "../uploads/1-8.png",
     "Ex_Len": "1000",
     "Ex_Width": "500",
+    "Ex_Height": "750",
+    "Unit": "个",
+    "RentPrice": "150元/个/会期",
+    "Deposit": "150元/个",
+    "Description": ""
+}
+, {
+
+    "KeyId": "9",
+    "Code": "1-9",
+    "Type": "zjl",
+    "Name": "问询桌",
+    "PicUrl": "../uploads/1-9.png",
+    "Ex_Len": "1000",
+    "Ex_Width": "450",
+    "Ex_Height": "750",
+    "Unit": "张",
+    "RentPrice": "150元/张/会期",
+    "Deposit": "150元/张",
+    "Description": ""
+}
+, {
+
+    "KeyId": "10",
+    "Code": "1-10",
+    "Type": "zjl",
+    "Name": "玻璃圆桌椅（1桌2椅）",
+    "PicUrl": "../uploads/1-10.png",
+    "Ex_Len": "600",
+    "Ex_Width": "520",
+    "Ex_Height": "",
+    "Unit": "套",
+    "RentPrice": "220元/套/会期",
+    "Deposit": "250元/套",
+    "Description": ""
+}
+, {
+
+    "KeyId": "11",
+    "Code": "1-11",
+    "Type": "zjl",
+    "Name": "条形桌",
+    "PicUrl": "../uploads/1-11.png",
+    "Ex_Len": "1800",
+    "Ex_Width": "450",
+    "Ex_Height": "800",
+    "Unit": "张",
+    "RentPrice": "200元/张/会期",
+    "Deposit": "300元/张",
+    "Description": ""
+}
+, {
+
+    "KeyId": "12",
+    "Code": "1-12",
+    "Type": "zjl",
+    "Name": "洽谈桌",
+    "PicUrl": "../uploads/1-12.png",
+    "Ex_Len": "700",
+    "Ex_Width": "700",
+    "Ex_Height": "700",
+    "Unit": "张",
+    "RentPrice": "100元/张/会期",
+    "Deposit": "150元/张",
+    "Description": ""
+}, {
+
+    "KeyId": "13",
+    "Code": "1-13",
+    "Type": "zjl",
+    "Name": "折叠椅",
+    "PicUrl": "../uploads/1-13.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "把",
+    "RentPrice": "20元/把/会期",
+    "Deposit": "50元/把",
+    "Description": ""
+}
+, {
+
+    "KeyId": "14",
+    "Code": "1-14",
+    "Type": "zjl",
+    "Name": "电视机",
+    "PicUrl": "../uploads/1-14.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "42英寸",
+    "Unit": "台",
+    "RentPrice": "1200元/台/会期",
+    "Deposit": "1200元/台",
+    "Description": ""
+}, {
+
+    "KeyId": "15",
+    "Code": "1-15",
+    "Type": "zjl",
+    "Name": "藤桌椅（一桌四椅）",
+    "PicUrl": "../uploads/1-15.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "套",
+    "RentPrice": "300元/套/会期",
+    "Deposit": "300元/套",
+    "Description": ""
+}
+, {
+
+    "KeyId": "17",
+    "Code": "1-17",
+    "Type": "zjl",
+    "Name": "塑料椅",
+    "PicUrl": "../uploads/1-17.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "把",
+    "RentPrice": "20元/把/会期",
+    "Deposit": "50元/把",
+    "Description": ""
+}
+, {
+
+    "KeyId": "18",
+    "Code": "1-18",
+    "Type": "zjl",
+    "Name": "黑色矮柜",
+    "PicUrl": "../uploads/1-18.png",
+    "Ex_Len": "1000",
+    "Ex_Width": "450",
     "Ex_Height": "1800",
     "Unit": "个",
     "RentPrice": "350元/个/会期",
-    "Deposit": "350元/个",
+    "Deposit": "300元/个",
+    "Description": ""
+}
+, {
+
+    "KeyId": "19",
+    "Code": "1-19",
+    "Type": "zjl",
+    "Name": "黑色高柜",
+    "PicUrl": "../uploads/1-19.png",
+    "Ex_Len": "1000",
+    "Ex_Width": "450",
+    "Ex_Height": "1800",
+    "Unit": "个",
+    "RentPrice": "350元/个/会期",
+    "Deposit": "300元/个",
+    "Description": ""
+}
+, {
+
+    "KeyId": "20",
+    "Code": "1-20",
+    "Type": "zjl",
+    "Name": "吧椅",
+    "PicUrl": "../uploads/1-20.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "个",
+    "RentPrice": "80元/个/会期",
+    "Deposit": "80元/个",
+    "Description": ""
+}
+, {
+
+    "KeyId": "21",
+    "Code": "1-21",
+    "Type": "zjl",
+    "Name": "长条桌",
+    "PicUrl": "../uploads/1-21.png",
+    "Ex_Len": "1.2",
+    "Ex_Width": "0.6",
+    "Ex_Height": "0.7",
+    "Unit": "张",
+    "RentPrice": "120元/张/会期",
+    "Deposit": "100元/张",
+    "Description": ""
+}
+, {
+
+    "KeyId": "22",
+    "Code": "1-22",
+    "Type": "zjl",
+    "Name": "梯步台",
+    "PicUrl": "../uploads/1-22.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "个",
+    "RentPrice": "200元/个/会期",
+    "Deposit": "200元/个",
+    "Description": ""
+}
+, {
+
+    "KeyId": "23",
+    "Code": "1-23",
+    "Type": "zjl",
+    "Name": "桌布",
+    "PicUrl": "../uploads/1-23.png",
+    "Ex_Len": "600",
+    "Ex_Width": "520",
+    "Ex_Height": "",
+    "Unit": "块",
+    "RentPrice": "60元/块/会期",
+    "Deposit": "40元/块",
+    "Description": ""
+}
+, {
+
+    "KeyId": "24",
+    "Code": "2-1",
+    "Type": "dj",
+    "Name": "轨道LED桶灯",
+    "PicUrl": "../uploads/2-1.png",
+    "Ex_Len": "220V/18w",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "盏",
+    "RentPrice": "100元/盏/会期",
+    "Deposit": "",
+    "Description": ""
+}
+, {
+
+    "KeyId": "25",
+    "Code": "2-2",
+    "Type": "zjl",
+    "Name": "轨道LED聚光灯",
+    "PicUrl": "../uploads/2-2.png",
+    "Ex_Len": "220V/38w",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "盏",
+    "RentPrice": "100元/盏/会期",
+    "Deposit": "",
+    "Description": ""
+}
+, {
+
+    "KeyId": "26",
+    "Code": "2-3",
+    "Type": "zjl",
+    "Name": "金卤灯",
+    "PicUrl": "../uploads/2-3.png",
+    "Ex_Len": "220V/60w",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "盏",
+    "RentPrice": "100元/盏/会期",
+    "Deposit": "",
+    "Description": ""
+}
+, {
+    
+    "KeyId": "27",
+    "Code": "2-4",
+    "Type": "zjl",
+
+    "PicUrl": "../uploads/2-4.png",
+    "Ex_Len": "220V/150w",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "盏",
+    "RentPrice": "50元/盏/会期",
+    "Deposit": "",
+    "Description": "",
+    "Name": "碘钨灯"
+
+}
+, {
+    "KeyId": "28",
+    "Code": "2-5",
+    "Type": "zjl",
+    "Name": "日光灯",
+    "PicUrl": "../uploads/2-5.png",
+    "Ex_Len": "220V/150w",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "盏",
+    "RentPrice": "50元/盏/会期",
+    "Deposit": "",
+    "Description": ""
+
+
+}
+, {
+    "KeyId": "29",
+    "Code": "2-6",
+    "Type": "zjl",
+    "Name": "条形插座",
+    "PicUrl": "../uploads/2-6.png",
+    "Ex_Len": "220V/10A",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "个",
+    "RentPrice": "50元/个/会期",
+    "Deposit": "",
+    "Description": ""
+
+}
+, {
+    "KeyId": "30",
+    "Code": "2-7",
+    "Type": "zjl",
+    "Name": "橡套电缆",
+    "PicUrl": "../uploads/2-7.png",
+    "Ex_Len": "4芯4平方",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "米",
+    "RentPrice": "15元/米/会期",
+    "Deposit": "",
+    "Description": ""
+
+  
+}
+, {
+    "KeyId": "31",
+    "Code": "2-8",
+    "Type": "zjl",
+    "Name": "橡套电缆",
+    "PicUrl": "../uploads/2-8.png",
+    "Ex_Len": "4芯6平方",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "米",
+    "RentPrice": "20元/米/会期",
+    "Deposit": "",
+    "Description": ""
+
+}
+, {
+    "KeyId": "32",
+    "Code": "2-9",
+    "Type": "zjl",
+    "Name": "白色护套线",
+    "PicUrl": "../uploads/2-9.png",
+    "Ex_Len": "2芯4平方",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "米",
+    "RentPrice": "15元/米/会期",
+    "Deposit": "",
+    "Description": ""
+}, {
+    "KeyId": "33",
+    "Code": "2-10",
+    "Type": "zjl",
+    "Name": "人字梯",
+    "PicUrl": "../uploads/2-10.png",
+    "Ex_Len": "铝合金2米",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "把",
+    "RentPrice": "200元/把/会期",
+    "Deposit": "",
+    "Description": ""
+
+}, {
+
+    "KeyId": "34",
+    "Code": "3-1",
+    "Type": "zjl",
+    "Name": "LED屏",
+    "PicUrl": "../uploads/3-1.png",
+    "Ex_Len": "0.5",
+    "Ex_Width": "",
+    "Ex_Height": "0.5",
+    "Unit": "平方",
+    "RentPrice": "800元/会期",
+    "Deposit": "2000元",
+    "Description": "P2"
+}, {
+    
+    "KeyId": "35",
+    "Code": "3-2",
+    "Type": "zjl",
+    "Name": "礼仪人员",
+    "PicUrl": "../uploads/3-2.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "",
+    "RentPrice": "400元/节次",
+    "Deposit": "",
+    "Description": "每次节为4个小时"
+
+},
+{
+        
+    "KeyId": "36",
+    "Code": "3-3",
+    "Type": "zjl",
+    "Name": "广告机",
+    "PicUrl": "../uploads/3-3.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "",
+    "RentPrice": "3000元/会期",
+    "Deposit": "3000元",
+    "Description": "尺寸：50寸  外观尺寸：1810*610*80mm  分辩率：1920*1080  功率：80W               材质：钢化玻璃，铝包边，冷轧板   颜色：银色，金色，白色  "
+
+ 
+}, {
+
+    "KeyId": "37",
+    "Code": "3-4",
+    "Type": "zjl",
+    "Name": "绿植",
+    "PicUrl": "../uploads/3-4.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "盆",
+    "RentPrice": "100元/盆/会期",
+    "Deposit": "50元/盆",
+    "Description": ""
+}, {
+    "KeyId": "38",
+    "Code": "3-5",
+    "Type": "zjl",
+    "Name": "鲜花",
+    "PicUrl": "../uploads/3-5.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "盆",
+    "RentPrice": "100元/盆/会期",
+    "Deposit": "50元/盆",
+    "Description": ""
+
+},
+{
+    "KeyId": "39",
+    "Code": "3-6",
+    "Type": "zjl",
+    "Name": "桌花",
+    "PicUrl": "../uploads/3-6.png",
+    "Ex_Len": "",
+    "Ex_Width": "",
+    "Ex_Height": "",
+    "Unit": "盆",
+    "RentPrice": "90元/盆/会期",
+    "Deposit": "50元/盆",
     "Description": ""
 }
 ]
